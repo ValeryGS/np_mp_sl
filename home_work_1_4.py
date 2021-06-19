7fbde25bc9eddcd77c6316ff1d8bc37997c67d63
ValeryGS <hdl-mebel@mail.ru>
2021-06-19 19:45:56 +0500

    Переименование переменной serial > place_n

--- home_work_1_4.py
+++ home_work_1_4.py
@@ -1,13 +1,13 @@
 num = int(input('Найдем максимальную цифру в числе. \n Введите число: '))
 res_n = 0
 count = 1
-serial = 0
+place_n = 0
 while num != 0:
     temp = num % 10
     if temp > res_n:
         res_n = temp
-        serial = count
+        place_n = count
     num = num //10
     count += 1
-print(f'Это {count - serial}-я цифра вчисле = {res_n}')
+print(f'Это {count - place_n}-я цифра вчисле = {res_n}')
 
