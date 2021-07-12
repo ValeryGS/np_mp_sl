9c4276075c7461001e6696a5f11fe98be55fda5d
ValeryGS <hdl-mebel@mail.ru>
2021-07-12 18:15:43 +0500

    broken code

--- home_work_7_2.py
+++ home_work_7_2.py
@@ -1,10 +1,38 @@
 # -------------------------- 2 ----------------------------
+from abc import ABC, abstractclassmethod
 
-class Clothes:
-    def __iter__(self,clothes):
-        self.clothes = clothes
-        pass
+
+class Clothes(ABC):
+    def __init__(self, size):
+        self.size = size
+
+    def cut(self):
+        self.vol = 0
+
     def __add__(self, other):
-        return Clothes(self.clothes + other.clothes)
+        return Clothes(self.vol + other.vol)
+
     def __str__(self):
-        return (f'{self.clothes}')
+        return f'{self.vol}'
+
+class Suit(Clothes):
+    def __init__(self, size):
+        self.size = size                # рост костюма
+    def cut(self):
+        self.vol = 2*size + 0.3
+        print(f'For one suit need to: {self.vol}')
+
+
+class Coat(Clothes):
+    def __init__(self, size):
+        self.size = size                # размер пальто
+
+    def cut(self):
+        self.vol = size/6.5 + 0.5
+        print(f'For one coat need to: {self.vol}')
+
+
+
+kimono = Suit(4)
+palto = Coat(56)
+print(f'{kimono + palto}')
