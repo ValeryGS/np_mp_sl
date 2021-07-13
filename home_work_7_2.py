4419ff83ee131f67aebb0387edcf95cd5427878e
ValeryGS <hdl-mebel@mail.ru>
2021-07-13 12:57:02 +0500

    it works but without summation

--- home_work_7_2.py
+++ home_work_7_2.py
@@ -1,38 +1,70 @@
 # -------------------------- 2 ----------------------------
-from abc import ABC, abstractclassmethod
+from abc import ABC, abstractmethod
 
 
 class Clothes(ABC):
-    def __init__(self, size):
-        self.size = size
+    def __init__(self):
+        pass
 
-    def cut(self):
-        self.vol = 0
+    @property
+    @abstractmethod
+    def cutting(self):
+        pass
 
     def __add__(self, other):
-        return Clothes(self.vol + other.vol)
+        return (self.cutting + other.cutting)
+        #return Clothes(self.cutting + other.cutting)
 
     def __str__(self):
-        return f'{self.vol}'
-
-class Suit(Clothes):
-    def __init__(self, size):
-        self.size = size                # рост костюма
-    def cut(self):
-        self.vol = 2*size + 0.3
-        print(f'For one suit need to: {self.vol}')
+        return f'{self.cutting}'
 
 
 class Coat(Clothes):
     def __init__(self, size):
-        self.size = size                # размер пальто
+        super().__init__()
+        self.size = size
 
-    def cut(self):
-        self.vol = size/6.5 + 0.5
-        print(f'For one coat need to: {self.vol}')
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, size):
+        # если у бабушки усы ...
+        self.__size = size
+
+    @property
+    def cutting(self):
+        return round(self.size / 6.4 * 0.5,2)
 
 
+class Suit(Clothes):
+    def __init__(self, growth):
+        super().__init__()
+        self.growth = growth
 
-kimono = Suit(4)
-palto = Coat(56)
-print(f'{kimono + palto}')
+    @property
+    def growth(self):
+        return self.__growth
+
+    @growth.setter
+    def growth(self, growth):
+        # усли утром встал не с той ноги ...
+        self.__growth = growth
+
+    @property
+    def cutting(self):
+        # self.result = (2 * self.growth + 0.3) / 100
+        return round((2 * self.growth + 0.3) / 100,2)
+
+
+coat = Coat(48)
+trench = Coat(56)
+suit = Suit(170)
+kimono = Suit(155)
+# красоту наводить уже нет ни сил ни времени
+print(coat)
+print(trench)
+print(kimono)
+print(suit)
+#print(coat + suit + trench)
