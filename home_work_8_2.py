188f56376fae1ade36aed715f268d73b531712a2
ValeryGS <hdl-mebel@mail.ru>
2021-07-14 11:03:15 +0500

    complited

---
+++ home_work_8_2.py
@@ -0,0 +1,23 @@
+class MyEx(Exception):
+    def __init__(self,text = f'\033[31m\033[1mDivision by zero is not possible.\033[0m'):
+        self.text = text
+        print(text)
+        pass
+
+
+
+try:
+    x = (int(input('Enter the number that we will divide: ')))
+    y = (int(input('Enter the number that will be the divisor: ')))
+    if y <= 0:
+        raise MyEx
+    res = x / y
+except ValueError:
+    print(f'\033[31m\033[1mThe values entered must be digits!\033[0m')
+except MyEx:
+    pass
+
+else:
+    print(f'The result of dividing {x} by {y} = {res}')
+finally:
+    print('To repeat the calculations, run the code again.')
