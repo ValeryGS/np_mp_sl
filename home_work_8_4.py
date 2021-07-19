a357c00d0fc5eb1a2bfe6daac6a27baf1847dc82
ValeryGS <hdl-mebel@mail.ru>
2021-07-19 15:06:00 +0500

    *

--- home_work_8_4.py
+++ home_work_8_4.py
@@ -21,13 +21,12 @@ class Equipment:
         self.t['model'] = model
         self.t['quantity'] = quantity
         self.cat = cat
-        # проверяем есть ли запись принтера и соответсвует ли модель
-
+        # проверяем есть ли запись предмета и соответсвует ли модель, если есть
+        # обновляем количество либо создаем новую запись
         if self.cat in store.keys():
             if store[self.cat]['brand'] == brand and store[self.cat]['model'] == model:
                 store[self.cat]['quantity'] += quantity
             else:
-                print(len(store),'test')
                 for i in range(1, len(store)+1):
                     if(self.cat + str(i))not in store.keys():
                         store[self.cat + str(i)] = self.t
@@ -35,24 +34,27 @@ class Equipment:
         else:
             store[self.cat] = self.t
 class Printer(Equipment):
-    def __init__(self, brand, model, quantity, cat):
-        super().__init__(brand, model, quantity, cat)
+    pass
+    # def __init__(self, brand, model, quantity, cat):
+    #     super().__init__(brand, model, quantity, cat)
 
 class Notebook(Equipment):
-    def __init__(self, brand, model, quantity, cat):
-        super().__init__(brand, model, quantity, cat)
-        #store['notebook'] = self.t
+    pass
+    # def __init__(self, brand, model, quantity, cat):
+    #     super().__init__(brand, model, quantity, cat)
+    #     #store['notebook'] = self.t
 
 class Scaner(Equipment):
-    def __init__(self, brand, model, quantity, cat):
-        super().__init__(brand, model, quantity, cat)
-        #store['scaner'] = self.t
+    pass
+    # def __init__(self, brand, model, quantity, cat):
+    #     super().__init__(brand, model, quantity, cat)
+    #     #store['scaner'] = self.t
 n1 = Notebook('acer','5210',1, 'notebook')
 n2 = Notebook('acer','5210',1, 'notebook')
 s1 = Scaner('hp','laserJ', 2, 'scaner')
 p1 = Printer('canon','x23', 1, 'printer')
 p2 = Printer('hp','gigo', 2, 'printer') #('ctx','c-11', 2)
-s1 = Scaner('sony','ninga', 2, 'scaner')
+s1 = Scaner('sony','ninja', 2, 'scaner')
 
 df = pd.DataFrame(store)
 print(df.head())
